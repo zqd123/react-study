b3c65164a0af2a5a34852b294525d289febef997
zhangqd <zhangqd@digiwin.com>
2026-03-17 09:19:45 +0800

    单独按钮计数

--- src/App.jsx
+++ src/App.jsx
@@ -5,59 +5,40 @@ import heroImg from './assets/hero.png'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const products = [
+    { title: '卷心菜', isFruit: false, id: 1 },
+  { title: '大蒜', isFruit: false, id: 2 },
+  { title: '苹果', isFruit: true, id: 3 },
+  ];
+  const listItems = products.map(product =>
+    <li key={product.id}>
+      {product.title}
+    </li>
+  );
 
+  
   return (
-    <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>开始</h1>
-          <p>
-            编辑 <code>src/App.jsx</code> 并保存以测试 <code>热更新</code>
-          </p>
-        </div>
-        <button
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          计数是 {count}
-        </button>
-      </section>
+    <div className='section'>
+      <img src={reactLogo}></img>
+      <br/>
+      <h1>欢迎来到我的react应用</h1>
+      <MyButton></MyButton>
+      <br/>
+      <MyButton></MyButton>
+      <ul className='list'>
+        {listItems}
+      </ul>
+    </div>
+  )
+}
 
-      <div className="ticks"></div>
-
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>文档</h2>
-          <p>你的问题, 答案</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                探索 Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                学习更多
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
-    </>
+function MyButton(){
+  const [count, setCount] = useState(0);
+  function handleClick(){
+    setCount(count + 1);
+  }
+  return (
+    <button onClick={handleClick}>按钮{count}</button>
   )
 }
 
